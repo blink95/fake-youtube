0efaaa9916be76695a94fa4cd6e555e246effcaf
Brendan Link <blink@uci.edu>
2018-09-21 17:52:49 -0700

    Added baseline .js with a test of disappearing text placeholder

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    /*$("#searchYT").css("color","blue");// all custom jQuery will go here*/
+    $("#test").keyup(function(){
+      $("#searchYT").css("color","rgba(255, 43, 43, 0)");
+    });
+});
